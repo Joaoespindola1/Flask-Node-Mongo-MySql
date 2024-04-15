1c05d72b207ac798d94fd109621dc71df6e73c66
João Pedro <joaopedro.libresolucoes@gmail.com>
2024-04-15 09:50:53 -0300

    Padrão MVC

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,5 +10,46 @@ exports.getAllUsers = async (req, res) => {
 };
 
 //create user
+
+exports.createUser = async (req,res) => {
+    try {
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        
+        await newUser.save()
+        res.status(200).json(newUser)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+};
+
 //update user
+
+exports.updateUser = async (req,res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        }, {
+            new: true
+        })
+        res.status(200).json(user)
+    } catch {
+        res.status(400).json({message: error.message})
+    }
+};
+
 //delete user
+
+exports.deleteUser = async (req,res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        res.status(200).json(user) 
+    }catch {
+        res.status(400).json({message: error.message})
+    }
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.get('/', userController.getAllUsers);
-//Vocês implementam os novos métodos atrelados a rota
+router.get('/list', userController.getAllUsers);
+
 //create
+
+router.post('/', userController.createUser);
+
 //update
+
+router.put('/:id', userController.updateUser);
+
 //delete
+
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
